84e9d447160981fc3ca5f179829a512643115703
TAdrah <124643296+TAdrah@users.noreply.github.com>
2023-04-18 19:31:40 -0700

    Update redfin_scraper.py

--- Data Entry & Manipulation/redfin: housing scraper/redfin_scraper.py
+++ Data Entry & Manipulation/redfin: housing scraper/redfin_scraper.py
@@ -1,9 +1,10 @@
 from bs4 import BeautifulSoup
+import re
 import requests
 
 
 class RedFin_Scraper:
-    def __init__(self):
+    def __init__(self, url):
         self.list_price = []
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/11"
@@ -13,7 +14,7 @@ class RedFin_Scraper:
                                "q=0.7"
         }
         self.response = requests.get(
-            url="https://www.redfin.com/city/13654/CA/Oakland",
+            url=url,
             headers=headers
         )
         self.soup = BeautifulSoup(self.response.text, "lxml")
@@ -45,3 +46,35 @@ class RedFin_Scraper:
             except AttributeError:
                 continue
         return addresses
+
+    def prices(self):
+        prices = []
+        for i in self.data:
+            try:
+                prices.append(i.find(class_='homecardV2Price').text)
+            except AttributeError:
+                continue
+        return prices
+
+    def bed_bath_sqft(self):
+        bbs = []
+        for i in self.data:
+            try:
+                text = i.find(class_='HomeStatsV2')
+                #filters out lots/acres by ensuring first character is a number
+                if text.text[0].isdigit():
+                    #uses regular expression to find numbers that may or may not have decimal
+                    beds = re.findall('\d+\.?\d*',text.findAll('div')[0].text)
+                    baths = re.findall('\d+\.?\d*',text.findAll('div')[1].text)
+                    sq_ft = re.findall('\d+\.?\d*',text.findAll('div')[2].text)
+
+                    if len(sq_ft)>1:
+                        #combines list as str and put it back to list to keep data uniform.
+                        sq_ft = list(sq_ft[0] + ',' + sq_ft[1])
+
+                    bbs.append((beds[0], baths[0], sq_ft[0]))
+
+            except AttributeError:
+                continue
+        print(bbs)
+        return bbs
